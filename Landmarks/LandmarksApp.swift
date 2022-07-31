0f1e72bf86f77ac50e1b1fd64c81c966074715c9
Francisco Arguedas <farguedas99@gmail.com>
2022-07-30 23:25:28 -0600

    First layout - Map with description

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Francisco Arguedas Garita on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
